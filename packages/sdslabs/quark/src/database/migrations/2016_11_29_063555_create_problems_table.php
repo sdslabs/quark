275f2506a77f40a87e411348da98d33bbda726b8
Arpit Singla <arpit270896@gmail.com>
2016-11-29 12:26:13 +0530

    Create Problems and Solutions table

---
+++ packages/sdslabs/quark/src/database/migrations/2016_11_29_063555_create_problems_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProblemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('problems', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100)->unique()->comment('To be shown in url');
+            $table->string('title', 100);
+            $table->text('description');
+            $table->integer('solution_id')->unsigned();
+            $table->integer('competition_id')->unsigned();
+            $table->integer('creator_id')->unsigned();
+            $table->integer('uploader_id')->unsigned();
+            $table->boolean('practice')->comment('If the problem is to be displayed in practice arena');
+            $table->integer('problem_type_id')->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+     
+            /* Foreign Keys */
+            // $table->foreign('solution_id')->references('id')->on('solutions');
+            // $table->foreign('competition_id')->references('id')->on('competitions');
+            // $table->foreign('creator_id')->references('id')->on('users');
+            // $table->foreign('uploader_id')->references('id')->on('users');
+            // $table->foreign('problem_type_id')->references('id')->on('problem_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('problems');
+    }
+}
---
+++ packages/sdslabs/quark/src/database/migrations/2016_11_29_064241_create_solutions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSolutionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('solutions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('practice_judge_id')->unsigned();
+            $table->integer('competition_judge_id')->unsigned();
+            $table->integer('practice_score')->unsigned();
+            $table->integer('competition_score')->unsigned();
+            $table->string('solution', 200)->comment("Hash in case of string/integer solution, file path in case of files");
+
+            /* Foreign Keys */
+            // $table->foreign('practice_judge_id')->references('id')->on('judges');
+            // $table->foreign('competition_judge_id')->references('id')->on('judges');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('solutions');
+    }
+}
