1e2ffa584480c8c182db6458a96f87e53faa89f6
Arpit Singla <arpit270896@gmail.com>
2016-11-29 12:23:56 +0530

    Create Users Table

---
+++ packages/sdslabs/quark/src/database/migrations/2016_11_29_063018_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unique();
+            $table->string('provider', 100);
+            $table->binary('credentials');
+            $table->string('username', 256)->unique();
+            $table->string('fullname', 256);
+            $table->string('email', 256)->unique();
+            $table->string('image', 256);
+            $table->integer('role_id')->unsigned();
+            $table->float('score')->default(0)->comment('Score in practice arena.');
+            $table->timestamps();
+
+            /* Foreign Keys */
+            // $table->foreign('role_id')->references('id')->on('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
