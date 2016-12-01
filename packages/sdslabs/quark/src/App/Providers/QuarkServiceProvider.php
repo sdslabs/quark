79033f5aaf1db48fe2a80ca3d0a61757d0e3bde9
Abhay <bhgupta13@gmail.com>
2016-12-01 16:19:01 +0530

    Changes in QuarkServiceProvider

--- packages/sdslabs/quark/src/App/Providers/QuarkServiceProvider.php
+++ packages/sdslabs/quark/src/App/Providers/QuarkServiceProvider.php
@@ -1,15 +1,11 @@
 <?php
-
 namespace SDSLabs\Quark\App\Providers;
-
 use SDSLabs\Quark\App\Auth\FalconGuard;
 use SDSLabs\Quark\App\Http\Middleware\Authenticate;
 use SDSLabs\Quark\App\Http\Middleware\Developer;
-
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Auth;
-
 class QuarkServiceProvider extends ServiceProvider
 {
     /**
@@ -17,33 +13,23 @@ class QuarkServiceProvider extends ServiceProvider
      *
      * @return void
      */
-
-    protected $router;
-
-
-    public function __construct(Router $router) {
-        $this->router = $router;
-    }
-
-    public function boot()
+    public function boot(Router $router)
     {
-        $this->setupRoutes();
+        $this->setupRoutes($router);
         $this->loadMigrationsFrom(__DIR__.'/../../database/migrations/');
-
         Auth::extend('falcon', function($app, $name, array $config) {
             return new FalconGuard;
         });
+        $router->middleWare('auth', Authenticate::class);
+        $router->middleWare('developer', Developer::class);
     }
-
     public function setupRoutes(Router $router)
     {
-        $this->router->group([
-            'namespace' => 'SDSLabs\Quark\App\Http\Controllers'
-        ], function($router) {
+        $router->group(['namespace' => 'SDSLabs\Quark\App\Http\Controllers'], function($router)
+        {
             require __DIR__.'/../Http/routes.php';
-        })->middleWare('auth', Authenticate::class)->middleWare('developer', Developer::class);
+        });
     }
-
     public function register()
     {
     }
