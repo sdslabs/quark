66309103459b86f4215920eb50ac853859782aa7
Arpit Singla <arpit270896@gmail.com>
2016-12-29 13:03:53 +0530

    Adds name and comments to routes

--- src/App/Http/routes.php
+++ src/App/Http/routes.php
@@ -1,27 +1,36 @@
 <?php
 
+// Problem Management Routes
 Route::resource('problems', 'ProblemController', ['except' => 'index', 'show']);
 
-Route::get('problems', 'PracticeProblemController@index');
-Route::get('problems/{problem}', 'PracticeProblemController@show');
-Route::post('problems/{problem}/submit', 'SubmissionController@practiceSubmission');
+// Practice Problem Routes
+Route::get('problems', 'PracticeProblemController@index')->name('practice.problems.index');
+Route::get('problems/{problem}', 'PracticeProblemController@show')->name('practice.problems.show');
+Route::post('problems/{problem}/submit', 'SubmissionController@practiceSubmission')->name('practice.problems.submit');
 
-Route::get('competitions/{competition}/problems', 'CompetitionProblemController@index');
-Route::get('competitions/{competition}/problems/{problem}', 'CompetitionProblemController@show');
-Route::post('competitions/{competition}/problems/{problem}/submit', 'SubmissionController@competitionSubmission');
+// Competition Problem Routes
+Route::get('competitions/{competition}/problems', 'CompetitionProblemController@index')->name('competitions.problems.index');
+Route::get('competitions/{competition}/problems/{problem}', 'CompetitionProblemController@show')->name('competitions.problems.show');
+Route::post('competitions/{competition}/problems/{problem}/submit', 'SubmissionController@competitionSubmission')->name('competitions.problems.submit');
 
+// Competition Team Routes
 Route::resource('competitions.teams', 'TeamController');
 
-Route::get('competitions/{competition}/leaderboard', 'CompetitionController@showLeaderboard');
-Route::get('competitions/{competition}/submissions', 'CompetitionController@showSubmissions');
+// Competition Leaderboard and Submission routes
+Route::get('competitions/{competition}/leaderboard', 'CompetitionController@showLeaderboard')->name('competitions.leaderboard.show');
+Route::get('competitions/{competition}/submissions', 'CompetitionController@showSubmissions')->name('competitions.submissions.show');
 
+// Competition Routes
 Route::resource('competitions', 'CompetitionController');
 
-Route::get('users/self', 'UserController@showSelf');
+// Use this route to check login status.
+Route::get('users/self', 'UserController@showSelf')->name('users.self.show');
+
+// User routes
 Route::resource('users', 'UserController', ['except' => ['destroy', 'create', 'store']]);
 
-
-Route::post('competitions/{competition}/teams/{team}/invite/{user}', 'CompetitionInvitesController@inviteUser');
-Route::post('competitions/{competition}/teams/{team}/join', 'CompetitionInvitesController@joinTeam');
-Route::get('/acceptInvite', 'CompetitionInvitesController@acceptInvite');
-Route::get('/cancelInvite', 'CompetitionInvitesController@cancelInvite');
+// Invite routes
+Route::post('competitions/{competition}/teams/{team}/invite/{user}', 'CompetitionInvitesController@inviteUser')->name('invites.send');
+Route::post('competitions/{competition}/teams/{team}/join', 'CompetitionInvitesController@joinTeam')->name('invites.join');
+Route::get('/acceptInvite', 'CompetitionInvitesController@acceptInvite')->name('invites.accept');
+Route::get('/cancelInvite', 'CompetitionInvitesController@cancelInvite')->name('invites.cancel');
