7b76bb1a5903892f39d0b4da38aae2e987499ae2
kartik-25 <k.p.2511998@gmail.com>
2017-06-15 14:43:49 +0530

    Added table for tags

---
+++ src/database/migrations/2017_06_15_042702_create_problem_tags_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProblemTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+		Schema::create('problem_tags', function (Blueprint $table) {
+			$table->increments('id');
+			$table->integer('problem_id');
+			$table->integer('tag_id');
+			$table->timestamps();
+		});
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+		Schema::dropIfExists('problem_tags');
+    }
+}
